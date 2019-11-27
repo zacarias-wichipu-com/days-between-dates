32c175bd106de7803ecd3412e6eb43be2a274282
Zacarías Calabria Liétor <zacarias@wichipu.com>
2019-11-27 19:43:28 +0100

    Add Clean and HTML Webpack plugins

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,8 @@
-console.log('¡Hola mundo!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Index = () => {
+  return <div>¡Hola mundo!</div>
+}
+
+ReactDOM.render(<Index/>, document.getElementById('index'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.jsx',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Days Between Dates'
+    })
+  ]
 }
