a1d41b20bb0817cb0f82fb9c18933e8c4ef6d98c
Zacarías Calabria Liétor <zacarias@wichipu.com>
2019-12-03 00:57:04 +0100

    Add modal

--- src/app.jsx
+++ src/app.jsx
@@ -2,15 +2,32 @@ import React, { useState } from 'react'
 import { render } from 'react-dom'
 import 'typeface-roboto'
 import DateFnsUtils from '@date-io/date-fns'
-import { Button, Container, Grid, Typography } from '@material-ui/core'
+import {
+  Backdrop,
+  Button,
+  Container,
+  Fade,
+  Grid,
+  Modal,
+  Typography
+} from '@material-ui/core'
 import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
 import './app.scss'
 
 const App = () => {
   const [selectedDate, setSelectedDate] = useState(new Date())
+  const [open, setOpen] = React.useState(false)
 
   const handleDateChange = date => {
     setSelectedDate(date)
+  }
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
   }
 
   return (
@@ -54,14 +71,31 @@ const App = () => {
               </div>
             </Grid>
             <Grid item xs>
-              <Button variant="contained" color="primary">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleOpen}>
                 Calculate
               </Button>
             </Grid>
           </form>
-          <div>
-            Result
-          </div>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500
+            }}>
+            <Fade in={open}>
+              <div>
+                <h2 id="transition-modal-title">Transition modal</h2>
+                <p id="transition-modal-description">react-transition-group animates me.</p>
+              </div>
+            </Fade>
+          </Modal>
         </Grid>
       </Container>
     </MuiPickersUtilsProvider>
