1c4311c9dceca7a780eb1e390228fb4934543e35
Zacarías Calabria Liétor <zacarias@wichipu.com>
2020-01-28 18:40:38 +0100

    Upgrade icon path.

--- app.js
+++ app.js
@@ -24773,7 +24773,7 @@ function Footer(props) {
     href: 'mailto:hablacon@wichipu.com'
   }, react_default.a.createElement(StyledCardMedia, {
     component: 'img',
-    src: './assets/images/wichipu.svg'
+    src: 'https://zacarias-wichipu-com.github.io/days-between-dates/assets/images/wichipu.svg'
   })));
 }
 // EXTERNAL MODULE: ./assets/css/app.scss
